f7d039a1363f391ae95477e7649b6afa14b40e04
Tatiane Gabrielle <shinoda_@hotmail.co.uk>
2020-06-16 18:15:43 -0300

    Terceira aula - create-point form ok

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -19,22 +19,66 @@ function getCities(event) {
 	stateInput.value = event.target.options[indexOfSelectedState].text
 
 	const url=`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
+	
+	citySelect.innerHTML= "<option value>Selecione a Cidade</option>"
+	citySelect.disabled = true
+	
 	fetch(url)
 	.then(res => res.json())
 	.then (cities => {
 		for (const city of cities) {
-			citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`  
+			citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`  
 		}
 		citySelect.disabled = false
 
 	})
 	
-}
-
-
-
-
-
+} 
 document
 	.querySelector("select[name=uf]")
 	.addEventListener("change", getCities)
+
+
+	//Itens de coleta //
+
+	const itemsToCollect = document.querySelectorAll(".items-grid li")
+	for (const item of itemsToCollect) {
+		item.addEventListener("click", handleSelectedItem)
+	}
+
+const collectedItems = document.querySelector("input[name=items")	
+let selectedItems =[]
+
+	function handleSelectedItem(event){
+		const itemLi = event.target
+
+		//add ou remove a classe ativa
+		itemLi.classList.toggle("selected")
+		const itemId = itemLi.dataset.id
+
+		//verificar se existem itens selecionados
+		//se sim, pegar itens selecionados
+		
+		const alreadySelected = selectedItems.findIndex(item => {
+			const itemFound = item === itemId //true ou false
+			return itemFound
+
+		})
+		//se tiver selecionado, tirar a seleção
+		if (alreadySelected >= 0){
+			//tirar seleção
+			const filteredItems = selectedItems.filter(item =>{
+				const itemIsDifferent = item != itemId
+				return itemIsDifferent
+			})
+
+			selectedItems = filteredItems
+
+		}else {
+			//se não estiver selecionado, adicionar
+			selectedItems.push(itemId)
+			
+		}
+		//atualizar o input com os itens
+			collectedItems.value = selectedItems
+	}
