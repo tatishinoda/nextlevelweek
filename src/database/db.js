f1e96c673654ba5bf92a3add307c90a378ea9de9
Tatiane Gabrielle <shinoda_@hotmail.co.uk>
2020-06-24 00:15:53 -0300

    Quinta Aula: SQLite, integração c/ BD - Site Final

---
+++ src/database/db.js
@@ -0,0 +1,79 @@
+//importar dependencia sqlite3
+const sqlite3 = require("sqlite3").verbose()
+
+//criar objeto que faz as operações no db
+
+const db = new sqlite3.Database ("./src/database/database.db")
+
+module.exports = db 
+//utilizar o objeto db para operações
+
+// db.serialize( ()=> {
+// 	//criar tabela
+// 	db.run(`
+// 			CREATE TABLE IF NOT EXISTS places (
+// 				id INTEGER PRIMARY KEY AUTOINCREMENT,
+// 				image TEXT,
+// 				name TEXT,
+// 				address TEXT,
+// 				address2 TEXT,
+// 				state TEXT,
+// 				city TEXT,
+// 				items TEXT
+// 			);
+// 	`)
+
+
+// 	//inserir na tabela
+// const query = `
+// 		INSERT INTO places (
+// 			image,
+// 			name,
+// 			address,
+// 			address2,
+// 			state,
+// 			city,
+// 			items
+// 		) VALUES (?,?,?,?,?,?,?);	`
+
+// 	const values = [
+// 		"https://images.unsplash.com/photo-1555606090-1640be5631c5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1225&q=80",
+// 		"Papersider",
+// 		"Guilherme Gemballa, Jardim América",
+// 		"Nº 260",
+// 		"Santa Catarina",
+// 		"Rio do Sul",
+// 		"Resíduos Eletronicos, Lâmpadas"
+// 	]
+
+// 	//depois de enviar os dados, ele exibe o resultado
+// 	function afterInsertData(err){
+// 		if(err){
+// 			return console.log(err)
+// 		}
+// 		console.log("Cadastrado com sucesso")
+// 		console.log(this)
+
+// 	}
+
+// 	db.run(query, values, afterInsertData)
+
+// 	//consultar dados na tabela
+// 	// db.all(`SELECT * FROM places`, function(err, rows){
+// 	// 	if(err){
+// 	// 		return console.log(err)
+// 	// 	}
+// 	// 	console.log("Aqui estão seus registros:")
+// 	// 	console.log(rows)
+// 	// })
+
+	//deletar dados na tabela
+	// db.run(`DELETE FROM places WHERE id = ?`, [1], function(err){
+	// 	if(err){
+	// 		return console.log(err)
+	// 	}
+	// 	console.log("Registro deletado com sucesso!")
+
+	// })
+
+// })
--- src/server.js
+++ src/server.js
@@ -2,9 +2,14 @@ const express = require("express")
 const server = express()
 
 
+// peger o bd
+const db = require("./database/db.js")
+
 //confg pasta publica
 server.use(express.static("public"))
 
+//habilitar req.body na app
+server.use(express.urlencoded({ extended: true}))
 
 //Template engine
 const nunjucks = require("nunjucks")
@@ -24,10 +29,73 @@ server.get("/", (req,res) => {
 	})
 })
 server.get("/create-point", (req,res) => {
+	//query strings da URL
+	console.log(req.query)
 	return res.render("create-point.html")
 })
+
+server.post("/savepoint", (req,res)=>{
+	
+	//req.pody - corpo do formulário
+	//console.log(req.body)
+
+
+	//inserir os dados no BD 
+		//inserir na tabela
+		const query = `
+				INSERT INTO places (
+					image,
+					name,
+					address,
+					address2,
+					state,
+					city,
+					items
+				) VALUES (?,?,?,?,?,?,?);	`
+		const values = [
+			req.body.image,
+			req.body.name,
+			req.body.address,
+			req.body.address2,
+			req.body.state,
+			req.body.city,
+			req.body.items
+		]
+
+	//depois de enviar os dados, ele exibe o resultado
+	function afterInsertData(err){
+		if(err){
+			return console.log(err)
+			return res.send("Erro no Cadastro")
+		}
+		console.log("Cadastrado com sucesso")
+		console.log(this)
+		//Retorna somente depois do cadastro
+		return res.render("create-point.html", {saved:true})
+	}
+
+	db.run(query, values, afterInsertData)
+})
+
 server.get("/search", (req,res) => {
-	return res.render("search-results.html")
+	
+	const search = req.query.search
+	if(search == ""){
+		//pesquisa vazia
+		//mostrar html com os dados do BD
+		return res.render("search-results.html", {total: 0})
+	}
+	//pegar os dados do BD
+	//% indica que aceita qualquer coisa que vier antes ou depois
+	db.all(`SELECT * FROM places WHERE city LIKE '%${search}%'`, function(err, rows){
+				if(err){
+					return console.log(err)
+				}
+				const total = rows.length
+
+				//mostrar html com os dados do BD
+				return res.render("search-results.html", {places : rows, total: total})
+			})	
 })
 
 //ligar o servidor
